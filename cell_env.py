d175311cb64ecf0e5d9954b4808b52e75c0df1b5
Jacob Adamczyk <42879357+JacobHA@users.noreply.github.com>
2024-08-06 23:44:23 -0400

    do framestacking, dt control, redefine state as cost

--- cell_env.py
+++ cell_env.py
@@ -4,38 +4,45 @@ import random
 from cell_model_pop_fde import Cell_Population
 
 class CellEnv(gym.Env):
-    def __init__(self, **kwargs):
+    def __init__(self, frame_stack=20, dt=0.15, **kwargs):
+        self.dt = dt
         # Use binary actions: apply antibiotic or not
         self.action_space = gym.spaces.Discrete(2)
         # Use continuous observations: cell population (or concentration)
-        self.observation_space = gym.spaces.Box(low=0, high=1, shape=(2,), dtype=np.float32)
+        self.observation_space = gym.spaces.Box(low=-1, high=1, shape=(frame_stack,), dtype=np.float32)
+        self.frame_stack = frame_stack
         # Initialize the cell population model
         self.cell_population = Cell_Population(**kwargs)
-        self.previous_concentration = None
+        # self.previous_cost = None
 
 
     def step(self, action):
         self.step_count += 1
-        old_state = self.previous_concentration
+        # old_cost = self.previous_cost
         # Apply antibiotic if action is 1
-        t, tot, cost, next_state = self.cell_population.simulate_population(action, delta_t=0.2)
-        next_state = float(next_state) / 1000
-        self.previous_concentration = next_state
+        t, tot, cost, p_final = self.cell_population.simulate_population(action, delta_t=self.dt, alpha_mem=0.7)
+        # next_state = float(next_state) #/ 1000
+        # self.previous_cost = cost
+        # todo: make this a deque:
+        self.stacked_states.pop(0)
+        self.stacked_states.append(cost)
         # Calculate reward
         reward = -cost
         # possible define termination signal if next_state=0 or above some threshold?
+        n_cells = tot
 
-        return np.array([old_state, next_state], dtype=np.float32), reward, False, False, {}
+        return np.array(self.stacked_states, dtype=np.float32), reward, False, False, {'n_cells': tot}
 
     def reset(self, seed=None):
         self.step_count = 0
         self.seed(seed)
         self.cell_population.initialize()
-        state, _ = self.cell_population.init_conditions
-        state = float(state) / 1000
-        self.previous_concentration = state
-
-        return np.array([state, state], dtype=np.float32), {}
+        state = 0.0# self.cell_population.init_conditions
+        n_cells = self.cell_population.init_conditions.sum()
+        # state = float(state) / 1000
+        self.previous_cost = state
+        self.stacked_states = [state] * self.frame_stack
+        return np.array(self.stacked_states, dtype=np.float32), {'n_cells': n_cells}
 
     def render(self, mode='human'):
         print(self.state)
