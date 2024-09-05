2495ef1f098eec345542844a85615b6582ef6dd2
JacobHA <jacobhadamczyk@gmail.com>
2024-09-05 01:27:04 +0000

    longer ddqn sweeps with updated params

--- wandb_sweep.py
+++ wandb_sweep.py
@@ -11,7 +11,7 @@ from gymnasium.wrappers import TimeLimit
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.dqn import DQN
-
+from ddqn import DoubleDQN
 
 int_hparams = {'train_freq', 'gradient_steps'}
 
@@ -23,20 +23,20 @@ except KeyError:
 
 
 DT = 0.01
+alpha = 0.8
 max_time = int(100 / DT)
 env_args = {
     'dt': DT,
-    'alpha_mem': 0.7,
+    'alpha_mem': alpha,
     'max_timesteps': max_time
 }    
 
 def main(log_dir='tf_logs', device='auto'):
-    total_timesteps = 200_000
+    total_timesteps = 300_000
     runs_per_hparam = 1
     avg_auc = 0
 
     for i in range(runs_per_hparam):
-        # unique_id = wandb.util.generate_id()[:-1] + f"{i}"
         with wandb.init(sync_tensorboard=True, 
                         # id=unique_id,
                         dir=WANDB_DIR,
@@ -50,24 +50,21 @@ def main(log_dir='tf_logs', device='auto'):
             wandb.log({'frames': frames})
             wandb.log({'terminating': 'True'})
             wandb.log({'truncating as terminate': 'True'})
-            gamma = 0.9995
-            wandb.log({'gamma': gamma})
+            wandb.log({'alpha': alpha})
             wandb.log({'dt': DT})
 
             env = Monitor(CellEnv(frame_stack=frames, **env_args))
-            # eval_env = Monitor(
-            #     DummyVecEnv([lambda: CellEnv(frame_stack=frames, **env_args)]))
             eval_env = Monitor(CellEnv(frame_stack=frames, **env_args))
 
             eval_callback = EvalCallback(eval_env, best_model_save_path=f'{WANDB_DIR}/sweep-models/{run.name}/',
                              n_eval_episodes=3,
-                             log_path='./rl-logs/', eval_freq=3000,
+                             log_path='./rl-logs/', eval_freq=10_000,
                              deterministic=True, render=False,
                              )
             # Choose the algo appropriately
-            agent = DQN('MlpPolicy',
-                        env, 
-                        gamma=gamma,
+            agent = DoubleDQN('MlpPolicy',
+                        env,
+                        train_freq=(1, "episode"),
                         **config,
                         device=device,
                         tensorboard_log=log_dir)
@@ -91,7 +88,7 @@ if __name__ == '__main__':
         # Run a hyperparameter sweep with W&B
         print("Running a sweep on W&B...")
 
-        sweep_id = 'jacobhadamczyk/iaifi-hackathon/dkev450q'  # Ensure this is the correct sweep ID
+        sweep_id = 'jacobhadamczyk/iaifi-hackathon/tysoozhs'  # Ensure this is the correct sweep ID
         wandb.agent(sweep_id, function=main, count=args.count)
         wandb.finish()
 
